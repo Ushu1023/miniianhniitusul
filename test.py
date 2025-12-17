d9feb5e6afbf483ddef1e5c427f9b959dcff8823
Ushu1023 <usukhbayargantulga0@gmail.com>
2025-12-17 12:02:28 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+def mean(numbers):
+    a = len(numbers)
+    b = sum(numbers)
+    return b /    a
+
+c = mean([1, 2, 3, 4, 5,8,9])
+print(c)
+{"result": c} # array , hashmap
+8 # integer
+"bataa" # String
+[1, 2, 3] # List<intger>
+["bataa", "tsogtoo"] # List<string>
